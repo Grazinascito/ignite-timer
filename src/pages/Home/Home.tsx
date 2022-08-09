0c21f23cda58cdfe5c861408565c02c1c045b288
Grazinascito <grazi.social84@gmail.com>
2022-08-09 11:33:44 -0300

    feat: add component routes

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,56 @@
+import { Play } from "phosphor-react";
+import {
+  CountDownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountDownButton,
+  TaskInput,
+} from "./styles";
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <FormContainer>
+        <form>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            type="text"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1"></option>
+            <option value="Projeto 2"></option>
+            <option value="Projeto 3"></option>
+            <option value="tomate"></option>
+          </datalist>
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            id="minutesAmount"
+            type="number"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+        </form>
+      </FormContainer>
+
+      <CountDownContainer>
+        <span>0</span>
+        <span>0</span>
+        <Separator>:</Separator>
+        <span>0</span>
+        <span>0</span>
+      </CountDownContainer>
+
+      <StartCountDownButton disabled type="submit">
+        <Play />
+        Começar
+      </StartCountDownButton>
+    </HomeContainer>
+  );
+}
